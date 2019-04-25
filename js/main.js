d875dce9c83b5ac089fda98e54ab9919b893e4fe
AldenMcGonagle <aldenmcgonagle@seattleacademy.org>
2019-04-25 10:20:31 -0700

    Fixed problem with different question types printing differently

--- js/main.js
+++ js/main.js
@@ -1,37 +1,72 @@
-<<<<<<< HEAD
     console.clear();
 
 
-    function makeQ(q, a, level) {
+    function makeQ(q, a, level, type) {
         let question = {}
         question.q = q;
         question.a = a;
         question.level = level;
+        question.type = type;
         return question;
     }
 
     let questions = [];
-    questions.push(makeQ("2^3", "8", 1));
-    questions.push(makeQ("\\dfrac{1}{2}", ".5", 1));
-    questions.push(makeQ("\\dfrac{1}{2}", ".5", 1));
-    questions.push(makeQ("\\sqrt{16}", "4", 1));
-    questions.push(makeQ("\\sqrt{16}=4", "True", 1));
+    questions.push(makeQ("2^3", "8", 1, 1));
+    questions.push(makeQ("\\dfrac{1}{2}", ".5", 1, 1));
+    questions.push(makeQ("\\sqrt{16}", "4", 1, 1));
+    questions.push(makeQ("\\sqrt{16}=4", "True", 1, 4));
+    questions.push(makeQ("How many Radians are in 360 degrees", "2\\pi", 3, 2))
+// For Question.type:
+//     1 = Number question
+//     2 = Plain text question
+//     3 = Text with picture question
+//     4 = True or false question
 
-
-    function makeTest(e) {
+    function makeTest(e) {      
         for (let i = 0; i < questions.length; i++) {
-            if (i % 2 == 1)
-                theQuestion = '<div class="odd">';
-            else
-                theQuestion = '<div class="even">';
-            theQuestion += '\\(';
-            theQuestion += questions[i].q;
-            theQuestion += '\\)';
-            theQuestion += '<input data-answer="';
-            theQuestion += questions[i].a;
-            theQuestion += '">'
-            theQuestion += '</div>';
-            $('#theQuestions').append(theQuestion);
+            if (questions[i].type == 1){
+                if (i % 2 == 1){
+                    theQuestion = '<div class="odd">';
+                }
+                else{
+                    theQuestion = '<div class="even">';
+                }
+                theQuestion += '\\(';
+                theQuestion += questions[i].q;
+                theQuestion += '\\)';
+                theQuestion += '<input data-answer="';
+                theQuestion += questions[i].a;
+                theQuestion += '">'
+                theQuestion += '</div>';
+                $('#theQuestions').append(theQuestion);
+             }
+            else if(questions[i].type == 2){
+                if (i % 2 == 1)
+                    theQuestion = '<div class="odd">';
+                else
+                    theQuestion = '<div class="even">';
+                theQuestion += questions[i].q;
+                theQuestion += '<input data-answer="';
+                theQuestion += questions[i].a;
+                theQuestion += '">'
+                theQuestion += '</div>';
+                $('#theQuestions').append(theQuestion);
+            }
+             else if(questions[i].type == 4){
+                if (i % 2 == 1)
+                    theQuestion = '<div class="odd">';
+                else
+                    theQuestion = '<div class="even">';
+                theQuestion += "True or False:"
+                theQuestion += '\\(';
+                theQuestion += questions[i].q;
+                theQuestion += '\\)';
+                theQuestion += '<input data-answer="';
+                theQuestion += questions[i].a;
+                theQuestion += '">'
+                theQuestion += '</div>';
+                $('#theQuestions').append(theQuestion);
+            }
         }
         MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
 
@@ -65,8 +100,3 @@
             $(this).removeClass("correct").addClass("incorrect");
         }
     }
-=======
-    questions.push(makeQ("2^3", "8", 1));
-
-    // makeTest();
->>>>>>> 8d1cd5c58461c00af0a391a6c8fa1170cb704958
